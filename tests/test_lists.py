9a74f49b389c0c86ec525f51c532aaac85670a3c
tkothih <159691208+tkothih@users.noreply.github.com>
2024-02-11 11:53:37 +0100

    calculate max works

--- tests/test_lists.py
+++ tests/test_lists.py
@@ -4,8 +4,13 @@ from typing import List
 
 
 # 1. assume the first value is the maximum
+# 2. iterate over the list
+# 3. compare biggest with the current element and if smaller update biggest
 def calculate_max(numbers: List[int]) -> int:
-    biggest = numbers[0]
+    biggest = numbers[0] # 1
+    for number in numbers: # 2
+        if biggest < number: # 3
+            biggest=number
     return biggest
 
 def test_calculate_max():
